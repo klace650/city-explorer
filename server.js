c828e5f0e134537fc73829986999d9ed1141b885
Klace Koch <koch.klace@gmail.com>
2020-10-17 14:27:31 -0700

    wrapped up feature 2

--- server.js
+++ server.js
@@ -11,22 +11,39 @@ app.use(cors());
 // ROUTES : .get (name of route, callback function (request/response))
 app.get('/', (request, response) => {response.send('hey world');});
 app.get('/location', locationHandler);
+app.get('/weather', weatherHandler);
 
+function weatherHandler(request, response){
+    let city = request.query.city;
+    let weatherArray = [];
+    let weatherData = require('./data/weather.json');
+    weatherData.data.forEach(forecast => {
+        let weatherForecast = new Weather (forecast);
+        weatherArray.push(weatherForecast);
+    });
+    response.send(weatherArray);
+};
 
 function locationHandler(request, response){
     let city = request.query.city;
-    let data = require('./server/data/location.json')[0];
+    let data = require('./data/location.json')[0];
     let location = new Location (data, city);
     response.send(location);
     console.log(city,data,location);
 }
 // Constructor to tailor our incoming raw data
 function Location (obj, query){
-    this.lat = obj.lat;
-    this.lon = obj.lon;
+    this.latitude = obj.lat;
+    this.longitude = obj.lon;
     this.search_query = query;
-    this.location = obj.display_name;
+    this.formatted_query = obj.display_name;
 }
+function Weather (obj){
+    this.forecast = obj.weatherForecast.description;
+    this.time = obj.datetime;
+
+};
+
 // Start our server!
 app.listen(PORT, ()=>{
     console.log(`Server is listening on port ${PORT}`)
