0d26beb43b8ae8a80c05dceed5da68078271be00
Klace Koch <koch.klace@gmail.com>
2020-10-24 18:31:01 -0700

    still not getting trail data to post to the page

--- server.js
+++ server.js
@@ -21,17 +21,22 @@ app.use(cors());
 // ROUTES
 app.get('/location', locationHandler);
 app.get('/weather', weatherHandler);
-// app.get('/trails', trailHander);
+app.get('/trails', trailHander);
 app.use('*', notFoundler);
 
 // -----------------------------------------
 // HANDLERS
+function notFoundler (request, response){
+  response.status(404).send('Client Error?');
+  // Do I need this?
+  console.log(request);
+}
 
 function locationHandler(request, response){
   let city = request.query.city;
   let key = process.env.LOCATION_API_KEY;
-
   const URL = `https://us1.locationiq.com/v1/search.php/?key=${key}&q=${city}&format=json`;
+
   superagent.get(URL)
     .then(data => {
       let location = new Location (data.body[0], city);
@@ -48,10 +53,11 @@ function weatherHandler(request, response){
   let lon = request.query.longitude;
   let key = process.env.WEATHER_API_KEY;
   const URL = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${key}`;
+
   superagent.get(URL)
     .then(data => {
-      let forecastArr = data.body.data.map(day => {
-        return new Weather(day);
+      let forecastArr = data.body.data.map(data => {
+        return new Weather(data);
       });
       response.send(forecastArr);
     })
@@ -60,26 +66,26 @@ function weatherHandler(request, response){
     })
 }
 
-// function trailHander (request, response){
-//   let lat = request.query.latitude;
-//   let lon = request.query.longitude;
-//   let key = process.env.TRAIL_API_KEY;
+function trailHander (request, response){
+  let lat = request.query.latitude;
+  let lon = request.query.longitude;
+  let key = process.env.TRAIL_API_KEY;
+  const URL= `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=50&key=${key}`;
+  // https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200955913-600f2c90e9a4c7c5e293cbff819aff3e
 
-//   const URL= `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=10&key=${key}`;
-//   superagent.get(URL).then(data =>{
-//     let trail = data.body.trails.map(trail =>
-//       new Trail(trail));
-//     response.status(200).send(trail);
-//   });
-// }
-// function serverError (request, response){
-//   response.status(500).send("Server Error");
-//   console.log(request);
-// }
-function notFoundler (request, response){
-  response.status(404).send('Client Error');
-  console.log(request);
+  superagent.get(URL)
+    .then(data =>{
+      let trailArr = data.body.trail.map(data => {
+        return new Trail(data);
+      });
+      console.log(trailArr);
+      response.send(trailArr);
+    })
+    .catch(error => {
+      response.status(500).send('Server Issue @ Trails');
+    });
 }
+
 
 // -------------------------------------------
 // CONSTRUCTORS
@@ -91,22 +97,20 @@ function Location (obj, query){
 }
 function Weather (obj){
   this.forecast = obj.weather.description;
-  // console.log(this.forecast);
-  // this.forecast pulls correct data - just isn't making it on the page//
   this.time = obj.datetime;
 }
-// function Trail (obj){
-//   this.name = obj.name;
-//   this.location = obj.location;
-//   this.length = obj.length;
-//   this.stars = obj.stars;
-//   this.star_votes = obj.starVotes;
-//   this.summary = obj.summary;
-//   this.trail_url = obj.url;
-//   this.conditions = obj.conditions;
-//   this.condition_date = obj.conditionDate;
-//   this.condition_time = obj.conditionTime;
-// }
+function Trail (obj){
+  this.name = obj.name;
+  this.location = obj.location;
+  this.length = obj.length;
+  this.stars = obj.stars;
+  this.star_votes = obj.starVotes;
+  this.summary = obj.summary;
+  this.trail_url = obj.url;
+  this.conditions = obj.conditions;
+  this.condition_date = obj.conditionDate;
+  this.condition_time = obj.conditionTime;
+}
 
 // -----------------------------------------
 // SERVER
