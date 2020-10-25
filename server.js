c2f2be432923a9c0cdcd86f34fcd84e07006beab
Klace Koch <koch.klace@gmail.com>
2020-10-24 17:12:51 -0700

    refactoring code to fit requirements of 3 labs....why did I do it like this?

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 'use strict'
-
 // -----------------------------------------
-// GLOBALS
+// DEPENDENCIES
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
 // const pg = require('pg');
-// const client = new pg.Client(process.env.DATABASE_URL);
-
+// -----------------------------------------
+// ENVIROMENT
 require('dotenv').config();
-
-
-
+// -----------------------------------------
+// POSTGRES
+// const client = new pg.Client(process.env.DATABASE_URL);
+// -----------------------------------------
+// APPLICATION
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.use(cors());
 
 // -----------------------------------------
 // ROUTES
 app.get('/location', locationHandler);
-app.get('/weather', weatherHandler);
-app.get('/trails', trailHander);
-
+// app.get('/weather', weatherHandler);
+// app.get('/trails', trailHander);
+app.use('*', notFoundler);
 
 // -----------------------------------------
 // HANDLERS
@@ -29,46 +30,60 @@ app.get('/trails', trailHander);
 function locationHandler(request, response){
   let city = request.query.city;
   let key = process.env.LOCATION_API_KEY;
+
   const URL = `https://us1.locationiq.com/v1/search.php/?key=${key}&q=${city}&format=json`;
-  superagent.get(URL).then(data => {
-    let location = new Location (data.body[0], city);
-    response.status(200).send(location);
-  })
-}
-function weatherHandler(request, response){
-  let lat = request.query.latitude;
-  let lon = request.query.longitude;
-  let key = process.env.WEATHER_API_KEY;
-  const URL = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${key}`;
-
-  superagent.get(URL).then(data => {
-    let forecast = data.body.data.map(forecast => {
-      new Weather (forecast);
-      return forecast;
-      // Line below logs correct data also - still isn't making it to the page.
-      // console.log(forecast.weather.description);
+  superagent.get(URL)
+    .then(data => {
+      let location = new Location (data.body[0], city);
+      response.status(200).send(location);
+      console.log('Searched City: ',city);
     })
-    response.status(200).send(forecast);
-  })
+    .catch(error => {
+      response.status(500).send('Server Issue @ Location')
+    })
 }
-function trailHander (request, response){
-  let lat = request.query.latitude;
-  let lon = request.query.longitude;
-  let key = process.env.TRAIL_API_KEY;
 
-  const URL= `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=10&key=${key}`;
-  superagent.get(URL).then(data =>{
-    let trail = data.body.trails.map(trail =>
-      new Trail(trail));
-    response.status(200).send(trail);
-  });
-}
-function serverError (request, response){
-  response.status(500).send("Server Error");
-  console.log(request);
-}
-function clientError (request, response){
-  response.status(404).send("Client Error");
+// function weatherHandler (request, response){
+//   const URL = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${key}`;
+// };
+// superagent.get(URL)
+//   .then( data => {
+//     let forecast = data.body.data.map(day => new Weather)
+//   })
+
+// function weatherHandler(request, response){
+//   let lat = request.query.latitude;
+//   let lon = request.query.longitude;
+//   let key = process.env.WEATHER_API_KEY;
+
+//   superagent.get(URL).then(data => {
+//     let forecast = data.body.data.map(forecast => {
+//       new Weather (forecast);
+//       return forecast;
+//       // Line below logs correct data also - still isn't making it to the page.
+//       // console.log(forecast.weather.description);
+//     })
+//     response.status(200).send(forecast);
+//   })
+// }
+// function trailHander (request, response){
+//   let lat = request.query.latitude;
+//   let lon = request.query.longitude;
+//   let key = process.env.TRAIL_API_KEY;
+
+//   const URL= `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=10&key=${key}`;
+//   superagent.get(URL).then(data =>{
+//     let trail = data.body.trails.map(trail =>
+//       new Trail(trail));
+//     response.status(200).send(trail);
+//   });
+// }
+// function serverError (request, response){
+//   response.status(500).send("Server Error");
+//   console.log(request);
+// }
+function notFoundler (request, response){
+  response.status(404).send('Client Error');
   console.log(request);
 }
 
@@ -86,18 +101,18 @@ function Weather (obj){
   // this.forecast pulls correct data - just isn't making it on the page//
   this.time = obj.datetime;
 }
-function Trail (obj){
-  this.name = obj.name;
-  this.location = obj.location;
-  this.length = obj.length;
-  this.stars = obj.stars;
-  this.star_votes = obj.starVotes;
-  this.summary = obj.summary;
-  this.trail_url = obj.url;
-  this.conditions = obj.conditions;
-  this.condition_date = obj.conditionDate;
-  this.condition_time = obj.conditionTime;
-}
+// function Trail (obj){
+//   this.name = obj.name;
+//   this.location = obj.location;
+//   this.length = obj.length;
+//   this.stars = obj.stars;
+//   this.star_votes = obj.starVotes;
+//   this.summary = obj.summary;
+//   this.trail_url = obj.url;
+//   this.conditions = obj.conditions;
+//   this.condition_date = obj.conditionDate;
+//   this.condition_time = obj.conditionTime;
+// }
 
 // -----------------------------------------
 // SERVER
